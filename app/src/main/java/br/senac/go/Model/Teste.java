59c3619db864069bfd0c51a7d92f782d9b628096
izaias.cintra <izaiasacintra@gmail.com>
2019-12-13 20:09:11 -0200

    commit teste izaias

---
+++ app/src/main/java/br/senac/go/Model/Teste.java
@@ -0,0 +1,13 @@
+package br.senac.go.Model;
+
+public class Teste {
+    String nome;
+
+    public Teste(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
